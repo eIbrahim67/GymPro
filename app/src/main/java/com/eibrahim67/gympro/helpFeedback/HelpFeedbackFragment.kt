a8edb2437e24b7a4de2ff7084a06da35bcd70d29
eIbrahim67 <ibrahim.mohamed.ibrahim.t@gmail.com>
2025-08-30 16:39:53 +0300

    add Help & Feedback page

--- app/src/main/java/com/eibrahim67/gympro/helpFeedback/HelpFeedbackFragment.kt
+++ app/src/main/java/com/eibrahim67/gympro/helpFeedback/HelpFeedbackFragment.kt
@@ -1,13 +1,19 @@
 package com.eibrahim67.gympro.helpFeedback
 
 import android.os.Bundle
+import android.text.Html
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import com.eibrahim67.gympro.databinding.FragmentHelpFeedbackBinding
+import kotlinx.coroutines.launch
 
 class HelpFeedbackFragment : Fragment() {
 
@@ -27,14 +33,28 @@ class HelpFeedbackFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Handle back button safely
+        observeViewModel()
+
         binding.backBtn.setOnClickListener {
             findNavController().popBackStack()
         }
 
-        // TODO: Interact with ViewModel here
     }
 
+    private fun observeViewModel() {
+        viewModel.loadHelpAndFeedbackContent()
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.helpAndFeedbackContent.collect { content ->
+                    try {
+                        binding.text.text = Html.fromHtml(content, Html.FROM_HTML_MODE_COMPACT)
+                    } catch (e: Exception) {
+                        Log.e("HelpFeedback", "Error updating description text: ${e.message}")
+                    }
+                }
+            }
+        }
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
