0244baa906c0f2dd43dd561d0769a4c4d1f68704
eIbrahim67 <ibrahim.mohamed.ibrahim.t@gmail.com>
2025-08-30 15:23:36 +0300

    solve overdraw warnings

--- app/src/main/java/com/eibrahim67/gympro/ocr/ApiOcrClient.kt
+++ app/src/main/java/com/eibrahim67/gympro/ocr/ApiOcrClient.kt
@@ -1,12 +1,13 @@
 package com.eibrahim67.gympro.ocr
 
+import com.eibrahim67.gympro.core.utils.VALS
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 
 object ApiOcrClient {
-    private const val BASE_URL = "http://"+ CONST_VALS.IP_CONFIG + ":3000/"
+    private const val BASE_URL = "http://"+ VALS.IP_CONFIG + ":3000/"
 
     val ocrApiService: OcrApiService by lazy {
         val retrofit = Retrofit.Builder()
